76cbf0ef5a88a608a7f6c8e0241b12f66c2f4c0e
orMaXbit <ormaxbit@gmail.com>
2023-02-28 23:31:16 +0300

    na proverku Lesson 2

---
+++ Lesson2Z10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int Res = 0;
+while(true)
+{
+    Console.Write("Vvedi 3-h znachnoe chislo: ");
+    int X = int.Parse(Console.ReadLine() ?? "");
+    if (X/100 != 0 && X/100 < 10)
+    { 
+        Res = (X%100)/10;
+        break;
+    }
+    Console.WriteLine("Oshibka! Chislo ne 3-h znachnoe!");
+}
+Console.Write($"2-ya cifra 3-h znacnogo chisla - {Res}");
---
+++ Lesson2Z13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int Res = 0;
+while(true)
+{
+    Console.Write("Vvedi chislo: ");
+    int X = int.Parse(Console.ReadLine() ?? "");
+    if (X/100 != 0)
+    { 
+        if (X/100 > 10)
+        {
+            X = X / 10;
+        }
+        Res = X%10;
+        break;
+    }
+    Console.WriteLine("Oshibka! Chislo menshe neobhodimogo razryada!");
+}
+Console.Write($"3-ya cifra chisla - {Res}");
